93af7e0d56d0f50c02f7e8add6e4572076ff9e4b
xglvdai <564188950@qq.com>
2020-05-25 08:43:37 +0800

    base.html

--- jobplus/app.py
+++ jobplus/app.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template
 from .config import configs
 from .models import db, User
 
+def register_blueprints(app):
+    from .handlers import front
+    app.register_blueprint(front)
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
     db.init_app(app)
+    register_blueprints(app)
     
-
-    @app.route('/')
-    def index():
-        return 'hello shiyanlou'
-
     return app
--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,1 +1,9 @@
-from import
+from flask import Blueprint, render_template
+
+
+front = Blueprint('front', __name__)
+
+
+@front.route('/')
+def index():
+    return render_template('index.html')
